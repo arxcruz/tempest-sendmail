47be44cdb302dd29ddaad0ba7c44134187c37113
Arx Cruz <arxcruz@redhat.com>
2017-03-20 12:04:18 +0100

    Updating wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-path = os.environ.get('OPENSHIFT_PYTHON_DIR', '~/')
+path = os.environ.get('OPENSHIFT_DATA_DIR', '~/')
 sys.path.insert(0, path)
 
 from tempest_sendmail import app as application
