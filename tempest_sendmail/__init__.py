87a00c69359b0487483fe028f478be0aaa542cbc
Arx Cruz <arxcruz@redhat.com>
2017-09-11 18:33:09 +0200

    Adding support to Gmail API

--- tempest_sendmail/__init__.py
+++ tempest_sendmail/__init__.py
@@ -1,4 +1,10 @@
+import base64
+import httplib2
+import oauth2client
+import os
 import smtplib
+
+from apiclient import errors, discovery
 
 from email.mime.text import MIMEText
 
@@ -9,6 +15,8 @@ from flask_limiter.util import get_remote_address
 from flask_restful import Api
 from flask_restful import Resource
 from flask_restful import reqparse
+
+from oauth2client import client, tools
 
 __version__ = '0.1'
 
@@ -27,25 +35,7 @@ request_parser.add_argument('mime_type')
 request_parser.add_argument('subject')
 
 
-class SendMailResource(Resource):
-    # @marshal_with(request_fields)
-    decorators = [limiter.limit('10/hour')]
-
-    def post(self):
-        args = request_parser.parse_args()
-        addresses = args['addresses']
-        print('ADDRESSES: {}'.format(addresses))
-        message = args['message']
-        mime_type = args['mime_type']
-        subject = args['subject']
-        mail = SendMail(addresses, message, mime_type, subject)
-        if mail.send_mail():
-            return 'OK', 200
-        else:
-            return 'Fail', 400
-
-
-class SendMail(object):
+class SendMailAbstract(object):
     def __init__(self, addresses, message, mime_type, subject):
         self._addresses = addresses
         self._message = message
@@ -70,7 +60,92 @@ class SendMail(object):
         return self._subject
 
     def send_mail(self):
+        pass
 
+
+class SendMailResource(Resource):
+    # @marshal_with(request_fields)
+    decorators = [limiter.limit('10/hour')]
+
+    def post(self):
+        args = request_parser.parse_args()
+        addresses = args['addresses']
+        print('ADDRESSES: {}'.format(addresses))
+        message = args['message']
+        mime_type = args['mime_type']
+        subject = args['subject']
+        mail_class = self._get_class_by_email(app.config['MAIL_FROM'])
+        mail = mail_class(addresses, message, mime_type, subject)
+        print('CLASS: {}'.format(mail.__class__))
+        if mail.send_mail():
+            return 'OK', 200
+        else:
+            return 'Fail', 400
+
+    def _get_class_by_email(self, email):
+        if 'redhat.com' in email:
+            return SendMailApi
+        return SendMail
+
+
+class SendMailApi(SendMailAbstract):
+    def __init__(self, addresses, message, mime_type, subject):
+        super(SendMailApi, self).__init__(addresses, message, mime_type,
+                                          subject)
+
+        self.credentials = self.get_credentials()
+
+    def get_credentials(self):
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir,
+                                       'gmail-python-email-send.json')
+        store = oauth2client.file.Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(
+                    os.path.join(
+                        credential_dir, app.config['CLIENT_SECRET_FILE']),
+                    app.config['SCOPES'])
+            flow.user_agent = app.config['APPLICATION_NAME']
+            credentials = tools.run_flow(flow, store)
+        return credentials
+
+    # def send_mail(self, sender, to, subject, msg_html, msg_plain):
+    def send_mail(self):
+        http = self.credentials.authorize(httplib2.Http())
+        service = discovery.build('gmail', 'v1', http=http)
+        message = self.create_message_html()
+        result = self.send_message_internal(service, "me", message)
+        return result
+
+    def send_message_internal(self, service, user_id, message):
+        try:
+            message = (service.users().messages().send(
+                userId=user_id, body=message).execute())
+            print 'Message Id: %s' % message['id']
+            return message
+        except errors.HttpError, error:
+            print 'An error occurred: %s' % error
+            return "Error"
+        return "OK"
+
+    def create_message_html(self):
+        msg = MIMEText(self.message, self.mime_type)
+        msg['Subject'] = self.subject
+        msg['From'] = self.mail_from
+        msg['To'] = ",".join(self.addresses)
+        return {'raw': base64.urlsafe_b64encode(msg.as_string())}
+
+
+class SendMail(SendMailAbstract):
+    def __init__(self, addresses, message, mime_type, subject):
+        super(SendMail, self).__init__(addresses, message,
+                                       mime_type, subject)
+
+    def send_mail(self):
         msg = MIMEText(self.message, self.mime_type)
         msg['Subject'] = self.subject
         msg['From'] = self.mail_from
@@ -90,3 +165,6 @@ class SendMail(object):
         return True
 
 api.add_resource(SendMailResource, '/api/v1.0/sendmail', endpoint='sendmail')
+
+if __name__ == '__main__':
+    sendmail = SendMailApi(['arxcruz@gmail.com'], 'test', 'html', 'test email api')
